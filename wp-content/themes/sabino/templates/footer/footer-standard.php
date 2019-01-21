878463d8cfd8840442e8ae01815ec954f6dc7e1f
Thanh Bui <lite.wall.r6@gmail.com>
2019-01-21 10:41:08 +0000

    remove watermark

--- wp-content/themes/sabino/templates/footer/footer-standard.php
+++ wp-content/themes/sabino/templates/footer/footer-standard.php
@@ -19,12 +19,6 @@
 	
 		<div class="site-container">
 			
-			<div class="site-footer-bottom-bar-left">
-			
-				<?php printf( __( 'Theme: %1$s by %2$s', 'sabino' ), __( '<a href="https://demo.kairaweb.com/#sabino">Sabino</a>', 'sabino' ), __( 'Kaira', 'sabino' ) ); ?>
-			
-			</div>
-			
 			<div class="site-footer-bottom-bar-right">
 			
 	            <?php wp_nav_menu( array( 'theme_location' => 'footer-bar','container' => false, 'fallback_cb' => false, 'depth'  => 1 ) ); ?>
